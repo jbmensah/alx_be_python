becc9cfaa34c533799248b9fb2c3075b8a1c792b
jbmensah <jaybmensah@gmail.com>
2024-06-22 10:22:08 +0000

    Practice basic arithmetic operations in Python by performing predefined calculations.

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+# Arithmetic operations in Python by performing predefined operations
+
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+difference = number1 - number2
+product =	number1 * number2
+
+# Print the sum
+print("Addtion of ", str(number1), "and ", str(number2), "is ", str(addition))
+
+# Print the difference
+print("Subtraction of ", str(number1), "and ", str(number2), "is ", str(difference))
+
+# Print the sum
+print("Multiplication of ", str(number1), "and ", str(number2), "is ", str(product))
