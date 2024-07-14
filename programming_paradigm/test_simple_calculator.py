913a67bc9465f432d8ef509d194dcc098b4447ae
jbmensah <jaybmensah@gmail.com>
2024-07-14 18:49:15 +0000

    Create a test_simple_calculator.py script to define and run unit tests for each method in the SimpleCalculator class.

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,7 +10,7 @@ class TestSimpleCalculator(unittest.TestCase):
 	def tearDown(self):
 		pass
 
-	def test_add(self):
+	def test_addition(self):
 		self.assertEqual(self.calc.add(2, 3), 5)
 		self.assertEqual(self.calc.add(-1, 1), 0)
 		self.assertEqual(self.calc.add(10, 5), 15)
